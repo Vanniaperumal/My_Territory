4aeb437f002109b2fa56464f6729427c1613b5b2
Vanniaperumal <g.vanniaperumal@gmail.com>
2023-06-10 00:06:38 +0530

    change done in local

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -22,6 +22,7 @@ public class ArmstrongNumber {
                 System.out.println("First branch Edit");
 		//This should be branch
 		//Edited in Github - 9th Jun 2023
+		//Edited in Local - 9thjun 2023
 
 		while(temp>0) {
 			a = temp%10;
